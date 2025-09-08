7251f0a848e4e3fef32f6b505a3185e8e7c2e26d
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-09-08 12:59:16 -0300

    Refactor EmailNotifier to use MailSender interface

--- src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
+++ src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
@@ -1,12 +1,23 @@
 package br.com.mariojp.solid.dip;
 
 public class EmailNotifier {
-	private final SmtpClient smtp = new SmtpClient();
+
+	private final MailSender mailSender;
+
+	// Construtor padrão que decide com base no DRY_RUN
+	public EmailNotifier() {
+		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
+			this.mailSender = new NoopMailSender();
+		} else {
+			this.mailSender = new SmtpMailSender();
+		}
+	}
+
+	public EmailNotifier(MailSender mailSender) {
+		this.mailSender = mailSender;
+	}
 
 	public void welcome(User user) {
-		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
-			// Estado inicial: ainda assim usa SMTP real (bug proposital)
-		}
-		smtp.send(user.email(), "Bem-vindo", "Olá " + user.name());
+		mailSender.send(user.email(), "Bem-vindo", "Olá " + user.name());
 	}
 }
